74a960b99ff4797a873afaa8a65d00e9196622fc
Abhishek Goyal <agoyal0308@gmail.com>
2024-03-12 22:26:50 +0530

    footer page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
 import Projects from "./pages/Projects";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 export default function Main() {
   return (
     <BrowserRouter>
@@ -19,6 +20,7 @@ export default function Main() {
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/projects" element={<Projects />}></Route>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
