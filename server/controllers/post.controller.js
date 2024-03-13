86f837a4a138c4a92f6ddff8c4ae878b51665239
Abhishek Goyal <agoyal0308@gmail.com>
2024-03-13 19:14:32 +0530

    create a post api route

---
+++ server/controllers/post.controller.js
@@ -0,0 +1,32 @@
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
+
+export const create = async (req, res, next) => {
+  console.log(req.body);
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, "you are not allowed to create a post"));
+  }
+  if (!req.body.title || !req.body.content) {
+    return next(errorHandler(400, "Please provide all required content"));
+  }
+  const slug = req.body.title
+    .split(" ")
+    .join("-")
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9-]/g, "-");
+  const newPost = new Post({
+    ...req.body,
+    slug,
+    userId: req.user.id,
+  });
+
+  try {
+    const savedPost = await newPost.save();
+    res.status(201).json({
+      success: true,
+      post: savedPost,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
