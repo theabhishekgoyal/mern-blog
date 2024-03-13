21c05acadc5d4f7b01f19a1f6b9b85a6eb935ff2
Abhishek Goyal <agoyal0308@gmail.com>
2024-03-14 00:05:43 +0530

    update some changes in getposts

--- server/index.js
+++ server/index.js
@@ -25,6 +25,8 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 
+
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
