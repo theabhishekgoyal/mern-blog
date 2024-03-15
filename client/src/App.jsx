bdd643ae86b768797c09e9fcea4a248d1f2478d5
Abhishek Goyal <agoyal0308@gmail.com>
2024-03-16 01:04:48 +0530

    adding project and about page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from "./pages/UpdatePost";
 import PostPage from "./pages/PostPage";
 import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 export default function Main() {
   return (
     <BrowserRouter>
@@ -24,6 +25,8 @@ export default function Main() {
         <Route path="/about" element={<About />}></Route>
         <Route path="/Sign-up" element={<SignUp />}></Route>
         <Route path="/Sign-in" element={<SignIn />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+
         <Route element={<PrivateRoute />}>
           <Route path="/dashboard" element={<Dashboard />} />
         </Route>
