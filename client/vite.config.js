4e8f0de898a036a42aca31fd3676a4e7f4f68884
Abhishek Goyal <agoyal0308@gmail.com>
2024-03-26 11:53:49 +0530

    vite target changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:3000",
+        target: "https://goyalblogs-api.onrender.com/",
         secure: false,
       },
     },
